6f0dd98f1235721ab8da64b78a665bcdcc356c42
Brian Muhia <iambrianmuhia@gmail.com>
2023-06-29 01:55:05 +0300

    add websocket to client object. increase max message size to 3MB

--- phx_events/client.py
+++ phx_events/client.py
@@ -287,7 +287,8 @@ class PHXChannelsClient:
         with self._executor_pool as pool:
             self.logger.debug('Connecting to websocket')
 
-            async with client.connect(self.channel_socket_url) as websocket:
+            async with client.connect(self.channel_socket_url, max_size=3000000) as websocket:
+                self.websocket = websocket
                 # Close the connection when receiving SIGTERM
                 shutdown_handler = partial(
                     self.shutdown,
